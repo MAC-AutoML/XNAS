8d27687ab1146dd54004107624d4a3cdd8dad510
zhengxiawu <zhengxiawu@126.com>
2020-07-01 11:05:38 +0800

    bug fixing

--- test/data_loader_test.py
+++ test/data_loader_test.py
@@ -4,9 +4,9 @@ from xnas.datasets.imagenet import XNAS_ImageFolder
 if __name__ == "__main__":
     for backend in ['torch', 'dali_cpu', 'dali_gpu', 'custom']:
         print('Testing the dataloader with backend:{}'.format(backend))
-        train_, val_ = XNAS_ImageFolder('/gdata/Caltech256/256_ObjectCategories',
-                                        [0.8, 0.2],
-                                        backend=backend)
+        [train_, val_] = XNAS_ImageFolder('/gdata/Caltech256/256_ObjectCategories',
+                                          [0.8, 0.2],
+                                          backend=backend)
 
         for i, (inputs, labels) in enumerate(train_):
             inputs = inputs.cuda()
