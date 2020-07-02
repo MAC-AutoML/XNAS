83028655e9c75752575546c1132dc31a6876aa8b
zhengxiawu <zhengxiawu@126.com>
2020-07-02 11:29:12 +0800

    debuging

--- test/data_loader_test.py
+++ test/data_loader_test.py
@@ -3,8 +3,6 @@ from xnas.datasets.imagenet import XNAS_ImageFolder
 
 if __name__ == "__main__":
     for backend in ['torch', 'dali_cpu', 'dali_gpu', 'custom']:
-        if backend in ['torch', 'dali_cpu', 'dali_gpu']:
-            continue
         print('Testing the dataloader with backend: {}'.format(backend))
         dataset = XNAS_ImageFolder('/gdata/Caltech256/256_ObjectCategories',
                                    [0.8, 0.2],
