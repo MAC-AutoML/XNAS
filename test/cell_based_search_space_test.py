9aca07483ae1a0b32609124833df720c6fdee641
zhengxiawu <zhengxiawu@126.com>
2020-07-07 10:32:44 +0800

    debug testing

--- test/cell_based_search_space_test.py
+++ test/cell_based_search_space_test.py
@@ -1,12 +1,12 @@
-from xnas.search_space.cell_based import DartsCNN
-from xnas.search_space.cell_based import NASBench201CNN
-from xnas.core.timer import Timer
-
-import torch
 import numpy as np
+import torch
 import torch.nn.functional as F
 
-if __name__ == "__main__":
+from xnas.core.timer import Timer
+from xnas.search_space.cell_based import DartsCNN, NASBench201CNN
+
+
+def basic_darts_cnn_test():
     # dartscnn test
     time_ = Timer()
     print("Testing darts CNN")
@@ -28,4 +28,32 @@ if __name__ == "__main__":
     time_.toc()
     print(_out_put.shape)
     print(time_.average_time)
+
+
+def basic_nas_bench_201_cnn():
+    #  nas_bench_201 test
+    time_ = Timer()
+    print("Testing darts CNN")
+    search_net = NASBench201CNN()
+    _random_architecture_weight = torch.randn(
+        [search_net.num_edges, len(search_net.basic_op_list)])
+    _input = torch.randn([2, 3, 32, 32])
+    time_.tic()
+    _out_put = search_net(_input, _random_architecture_weight)
+    time_.toc()
+    print(_out_put.shape)
+    print(time_.average_time)
+    time_.reset()
+    _random_one_hot = torch.Tensor(np.eye(len(search_net.basic_op_list))[
+                                   np.random.choice(len(search_net.basic_op_list), search_net.num_edges)])
+    _input = torch.randn([2, 3, 32, 32])
+    time_.tic()
+    _out_put = search_net(_input, _random_one_hot)
+    time_.toc()
+    print(_out_put.shape)
+    print(time_.average_time)
+
+
+if __name__ == "__main__":
+    basic_nas_bench_201_cnn()
     pass
