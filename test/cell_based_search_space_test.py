c189cc4506b538a6134b896d2342c42a8acbdc12
zhengxiawu <zhengxiawu@126.com>
2020-07-07 10:14:14 +0800

    testing

--- test/cell_based_search_space_test.py
+++ test/cell_based_search_space_test.py
@@ -1,5 +1,6 @@
 from xnas.search_space.cell_based import DartsCNN
 from xnas.search_space.cell_based import NASBench201CNN
+from xnas.core.timer import Timer
 
 import torch
 import numpy as np
@@ -7,9 +8,24 @@ import torch.nn.functional as F
 
 if __name__ == "__main__":
     # dartscnn test
+    time_ = Timer()
+    print("Testing darts CNN")
     search_net = DartsCNN()
-    _random_architecture_weight = torch.randn([search_net.num_edges * 2, len(search_net.basic_op_list)])
+    _random_architecture_weight = torch.randn(
+        [search_net.num_edges * 2, len(search_net.basic_op_list)])
     _input = torch.randn([2, 3, 32, 32])
+    time_.tic()
     _out_put = search_net(_input, _random_architecture_weight)
+    time_.toc()
     print(_out_put.shape)
+    print(time_.average_time)
+    time_.reset()
+    _random_one_hot = torch.Tensor(np.eye(len(search_net.basic_op_list))[
+                                   np.random.choice(len(search_net.basic_op_list), search_net.num_edges * 2)])
+    _input = torch.randn([2, 3, 32, 32])
+    time_.tic()
+    _out_put = search_net(_input, _random_architecture_weight)
+    time_.toc()
+    print(_out_put.shape)
+    print(time_.average_time)
     pass
