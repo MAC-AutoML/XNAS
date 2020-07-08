95e374717c8233500f35577d214d35ad384ed02d
zhengxiawu <zhengxiawu@126.com>
2020-07-08 10:59:46 +0800

    update test

--- test/data_loader_test.py
+++ test/data_loader_test.py
@@ -27,6 +27,8 @@ def image_folder_test():
 
 
 def cifar10_test():
+    from xnas.core.config import cfg
+    cfg.TRAIN.IM_SIZE = 32
     [train_, val_] = Xnas_Cifar10('/gdata/cifar10/cifar-10-batches-py', [0.8, 0.2])
     for i, (inputs, labels) in enumerate(train_):
         inputs = inputs.cuda()
