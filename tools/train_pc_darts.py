2a0f7d75e97502b9a7c1d7a6e77b86c8a80083e7
woodchen7 <1004463561@qq.com>
2020-07-22 16:41:14 +0800

    updata

--- tools/train_pc_darts.py
+++ tools/train_pc_darts.py
@@ -1,7 +1,7 @@
 import os
 import gc
 import sys
-sys.path.append("..")
+sys.path.append(".")
 import xnas.core.checkpoint as checkpoint
 import xnas.core.config as config
 import xnas.core.logging as logging
