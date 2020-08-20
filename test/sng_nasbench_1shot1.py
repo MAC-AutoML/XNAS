5c3266ddd0e79cc1db47a154e0a54db178fe3f51
ShaokunZhang <shaokunzhang529@gmail.com>
2020-08-21 01:28:58 +0800

    add nasbench1shot1_test

--- test/sng_nasbench_1shot1.py
+++ test/sng_nasbench_1shot1.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append(".")
 import argparse
 import copy
 import os
@@ -22,25 +24,25 @@ from xnas.search_space.test_function import (EpochSumCategoryTestFunction,
 from xnas.core.utils import index_to_one_hot, one_hot_to_index
 
 
-def get_optimizer(name, category, step=4, gamma=0.9, sample_with_prob=True, utility_function='log', utility_function_hyper=0.4):
+def get_optimizer(name, category, step = 4, gamma = 0.9, sample_with_prob=True, utility_function = 'log', utility_function_hyper = 0.4):
     if name == 'DDPNAS':
         return CategoricalDDPNAS(category, 3)
     elif name == 'MDENAS':
         return CategoricalMDENAS(category, 0.01)
     elif name == 'SNG':
-        return SNG(categories=category)
+        return SNG(categories = category)
     elif name == 'ASNG':
-        return ASNG(categories=category)
+        return ASNG(categories = category)
     elif name == 'dynamic_ASNG':
-        return Dynamic_ASNG(categories=category, step=step, pruning=True, sample_with_prob=sample_with_prob)
+        return Dynamic_ASNG(categories = category, step = step, pruning = True, sample_with_prob = sample_with_prob)
     elif name == 'dynamic_SNG':
-        return Dynamic_SNG(categories=category, step=step,
-                           pruning=True, sample_with_prob=sample_with_prob)
+        return Dynamic_SNG(categories = category, step = step,
+                           pruning = True, sample_with_prob = sample_with_prob)
     elif name == 'MIGO':
-        return MIGO(categories=category, step=step,
-                    pruning=True, sample_with_prob=sample_with_prob,
-                    utility_function='log', utility_function_hyper=utility_function_hyper,
-                    momentum=True, gamma=gamma)
+        return MIGO(categories = category, step = step,
+                    pruning = True, sample_with_prob = sample_with_prob,
+                    utility_function = 'log', utility_function_hyper = utility_function_hyper,
+                    momentum = True, gamma = gamma)
     elif name == 'GridSearch':
         return GridSearch(category)
     else:
@@ -55,24 +57,25 @@ class Reward(object):
         self.budget = budget
 
     def compute_reward(self, sample):
-        config = ConfigSpace.Configuration(self.space.get_configuration_space(), vector=sample)
-        y, c = self.space.objective_function(self.nasbench, config, budget=self.budget)
+        config = ConfigSpace.Configuration(self.space.get_configuration_space(), vector = sample)
+        y, c = self.space.objective_function(self.nasbench, config, budget = self.budget)
         fitness = float(y)
         return fitness
+
     def get_accuracy(self,sample):
-        #return test_accuracy of sample
-        config=ConfigSpace.Configuration(self.space.get_configuration_space(), vector=sample)
+        #return test_accuracy of a sample
+        config = ConfigSpace.Configuration(self.space.get_configuration_space(), vector = sample)
         adjacency_matrix, node_list = self.space.convert_config_to_nasbench_format(config)
         node_list = [INPUT, *node_list, OUTPUT] if self.space.search_space_number == 3 else [INPUT, *node_list, CONV1X1, OUTPUT]
         adjacency_list = adjacency_matrix.astype(np.int).tolist()
-        model_spec = api.ModelSpec(matrix=adjacency_list, ops=node_list)
-        nasbench_data = self.nasbench.query(model_spec, epochs=self.budget)
+        model_spec = api.ModelSpec(matrix = adjacency_list, ops = node_list)
+        nasbench_data = self.nasbench.query(model_spec, epochs = self.budget)
         return nasbench_data['test_accuracy']
 
 
-def run(space=1, optimizer_name='SNG', budget=108, runing_times=500, runing_epochs=200,
-        step=4, gamma=0.9, save_dir=None, nasbench=None, noise=0.0, sample_with_prob=True, utility_function='log',
-        utility_function_hyper=0.4):
+def run(space = 1, optimizer_name = 'SNG', budget = 108, runing_times = 500, runing_epochs = 200,
+        step = 4, gamma = 0.9, save_dir = None, nasbench = None, noise = 0.0, sample_with_prob = True, utility_function = 'log',
+        utility_function_hyper = 0.4):
     print('##### Search Space {} #####'.format(space))
     search_space = eval('SearchSpace{}()'.format(space))
     cat_variables = []
@@ -82,12 +85,12 @@ def run(space=1, optimizer_name='SNG', budget=108, runing_times=500, runing_epoc
             cat_variables.append(len(h.choices))
 
     # get category using cat_variables
-    category=cat_variables
+    category = cat_variables
 
     # distribution_optimizer = Category_DDPNAS.CategoricalDDPNAS(category, 3)
-    distribution_optimizer = get_optimizer(optimizer_name, category, step=step, gamma=gamma,
-                                           sample_with_prob=sample_with_prob, utility_function=utility_function,
-                                           utility_function_hyper=utility_function_hyper)
+    distribution_optimizer = get_optimizer(optimizer_name, category, step = step, gamma = gamma,
+                                           sample_with_prob = sample_with_prob, utility_function = utility_function,
+                                           utility_function_hyper = utility_function_hyper)
     
     # path to save the test_accuracy
     file_name = '{}_{}_{}_{}_{}_{}_{}_{}_{}.npz'.format(optimizer_name, str(space), str(runing_epochs),
@@ -105,9 +108,6 @@ def run(space=1, optimizer_name='SNG', budget=108, runing_times=500, runing_epoc
     test_accuracy = 0
     for i in tqdm.tqdm(range(runing_times)):
 
-        step=0
-        rtime=[]
-        rrecord=[]
 
         for j in range(runing_epochs):
             start_time = time.time()
@@ -118,7 +118,7 @@ def run(space=1, optimizer_name='SNG', budget=108, runing_times=500, runing_epoc
                     break
 
             sample = distribution_optimizer.sampling()
-            sample_index=one_hot_to_index(np.array(sample))
+            sample_index = one_hot_to_index(np.array(sample))
             validation_accuracy = nb_reward.compute_reward(sample_index)
             distribution_optimizer.record_information(sample, validation_accuracy)
             distribution_optimizer.update()
@@ -128,12 +128,6 @@ def run(space=1, optimizer_name='SNG', budget=108, runing_times=500, runing_epoc
             record['test_accuracy'][i, j] = test_accuracy
             end_time = time.time()
             running_time_interval[i, j] = end_time - start_time
-
-            rtime.append(step)
-            step+=1
-            rrecord.append(test_accuracy)
-        plt.plot(np.array(rtime),np.array(rrecord))
-        plt.show()
         
         del distribution_optimizer
         distribution_optimizer = get_optimizer(optimizer_name, category, step=step, gamma=gamma,
@@ -146,31 +140,30 @@ def run(space=1, optimizer_name='SNG', budget=108, runing_times=500, runing_epoc
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--space", help="search space name in [1,2,3]",type=int, default=1)
-    parser.add_argument("--optimizer", help="dicrete level", type=str, default='SNG')
-    parser.add_argument("--step", help="pruning step", type=int, default=4)
-    parser.add_argument("--gamma", help="gamma value", type=float, default=0.9)
-    parser.add_argument("--noise", help="noise std", type=float, default=0.0)
+    parser.add_argument("--space", help = "search space name in [1,2,3]",type = int, default = 1)
+    parser.add_argument("--optimizer", help = "dicrete level", type = str, default = 'SNG')
+    parser.add_argument("--step", help = "pruning step", type = int, default = 4)
+    parser.add_argument("--gamma", help = "gamma value", type = float, default = 0.9)
+    parser.add_argument("--noise", help = "noise std", type = float, default = 0.0)
     parser.add_argument("-uh", "--utility_function_hyper",
-                        help="the factor of utility_function", type=float, default=0.4)
-    parser.add_argument("-ut", "--utility_function_type", help="the type of utility_function", type=str, default='log')
-    parser.add_argument("-sp", "--sample_with_prob",  action='store_true')
+                        help = "the factor of utility_function", type = float, default = 0.4)
+    parser.add_argument("-ut", "--utility_function_type", help = "the type of utility_function", type = str, default = 'log')
+    parser.add_argument("-sp", "--sample_with_prob",  action = 'store_true')
     args = parser.parse_args()
 
     #get nasbench
-    nasbench_path='/Users/zhangshaokun/Projects/XNAS/nasbench_record/nasbench_full.tfrecord'
-    nasbench=api.NASBench(nasbench_path)
+    nasbench_path = '/PATH/TO/NASBENCH/nasbench_full.tfrecord'
+    nasbench = api.NASBench(nasbench_path)
 
     #get args
     space = args.space
     step = args.step
     gamma = args.gamma
-    save_dir = '/Users/zhangshaokun/Projects/XNAS/experiment'
-    nasbench_path='/Users/zhangshaokun/Projects/XNAS/nasbench_record/nasbench_full.tfrecord'
+    save_dir = '/PATH/TO/EXPERIMENT'
     optimizer_name = args.optimizer
 
-    print("space={}, step={}, gamma={}, optimizer={}, noise_std={}, utility_function_hyper={}, utility_function_type={}, sample_with_prob={}".format(
+    print("space = {}, step = {}, gamma = {}, optimizer = {}, noise_std = {}, utility_function_hyper = {}, utility_function_type = {}, sample_with_prob = {}".format(
         str(space), str(step), str(gamma), optimizer_name, str(args.noise), str(args.utility_function_hyper), args.utility_function_type, str(args.sample_with_prob)))
-    run(space, optimizer_name=optimizer_name, runing_times=1, runing_epochs=100,
-        step=step, gamma=gamma, save_dir=save_dir, nasbench=nasbench, noise=args.noise, sample_with_prob=args.sample_with_prob,
-        utility_function=args.utility_function_type, utility_function_hyper=args.utility_function_hyper)
+    run(space, optimizer_name = optimizer_name, runing_times = 1, runing_epochs = 100,
+        step = step, gamma = gamma, save_dir = save_dir, nasbench = nasbench, noise = args.noise, sample_with_prob = args.sample_with_prob,
+        utility_function = args.utility_function_type, utility_function_hyper = args.utility_function_hyper)
