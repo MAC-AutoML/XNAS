a7fec18a8f15b087eebfa5863ab00c2b701701de
zhengxiawu <zhengxiawu@126.com>
2020-09-24 20:51:18 +0800

    update example

--- test/architecture_plot.py
+++ test/architecture_plot.py
@@ -1,6 +1,9 @@
 from xnas.core.plotting import graph_plot
 
 if __name__ == "__main__":
-    genotype = '|nor_conv_3x3~0|+|nor_conv_1x1~0|avg_pool_3x3~1|+|skip_connect~0|nor_conv_3x3~1|skip_connect~2|'
+    #     genotype = '|nor_conv_3x3~0|+|nor_conv_1x1~0|nor_conv_3x3~1|+|skip_connect~0|nor_conv_3x3~1|skip_connect~2|'
+    #     file_path = './experiment/a.png'
+    #     graph_plot(genotype, file_path, caption=None, search_space='nasbench_201', n_nodes=3)
+    genotype = '|conv_3x3~0|+|conv_1x1~0|conv_1x1~1|+|none~0|conv_1x1~1|conv_1x1~2|+|conv_3x3~0|conv_3x3~1|none~2|none~3|'
     file_path = './experiment/a.png'
-    graph_plot(genotype, file_path, caption=None, search_space='nasbench_201', n_nodes=3)
+    graph_plot(genotype, file_path, caption=None, search_space='nasbench_201', n_nodes=4)
